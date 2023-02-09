1d2182e5e033c53a6c54a7fa5ef95ec1d8dc0a3e
Caio Pereira Antunes <caioefg@gmail.com>
2023-02-09 12:18:57 -0300

    Feat: criando função (req7,8,9)

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,13 +1,35 @@
+from datetime import datetime
+from tech_news.database import search_news
+
+
 # Requisito 7
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    links = []
+    query = {"title": {"$regex": title, "$options": 'i'}}
+
+    for item in search_news(query):
+        links.append((item["title"], item["url"]))
+    return links
 
 
 # Requisito 8
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    links = []
+    try:
+        date = datetime.strptime(date, "%Y-%m-%d").strftime("%d/%m/%Y")
+        for item in search_news({"timestamp": date}):
+            links.append((item["title"], item["url"]))
+    except ValueError:
+        raise ValueError("Data inválida")
+    return links
 
 
 # Requisito 9
 def search_by_category(category):
-    """Seu código deve vir aqui"""
+    query = {"category": {"$regex": category, "$options": "i"}}
+    links = []
+
+    for item in search_news(query):
+        links.append((item["title"], item["url"]))
+
+    return links
